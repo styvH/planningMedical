27e5ab0d4c1922e21f62881d6139b838962d5639
styvH <100362675+styvH@users.noreply.github.com>
2022-05-24 16:45:22 -0400

    MAJ databaase

--- planning_medical.sql
+++ planning_medical.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 08 avr. 2022 à 16:16
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Généré le : mar. 24 mai 2022 à 20:44
+-- Version du serveur : 8.0.27
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,18 +29,38 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `activite`;
 CREATE TABLE IF NOT EXISTS `activite` (
-  `Id_ACTIVITE` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `nombreMedecinActuel` int(3) NOT NULL DEFAULT '0',
+  `Id_ACTIVITE` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `heure_debut` int NOT NULL,
+  `heure_fin` int NOT NULL,
+  `id_journee` int NOT NULL,
+  `nombreMedecinActuel` int NOT NULL DEFAULT '0',
   `renouveler` tinyint(1) NOT NULL DEFAULT '0',
   `derniere_minute` tinyint(1) NOT NULL DEFAULT '0',
   `valide` tinyint(1) DEFAULT '0',
-  `Id_Medecin` int(11) DEFAULT NULL,
-  `Id_TYPE_ACTIVITE` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Id_Medecin` int DEFAULT NULL,
+  `Id_TYPE_ACTIVITE` varchar(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`Id_ACTIVITE`),
   KEY `Id_Medecin` (`Id_Medecin`),
-  KEY `Id_TYPE_ACTIVITE` (`Id_TYPE_ACTIVITE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `Id_TYPE_ACTIVITE` (`Id_TYPE_ACTIVITE`),
+  KEY `id_jour_debut` (`id_journee`),
+  KEY `heure_debut` (`heure_debut`),
+  KEY `heure_fin` (`heure_fin`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `activite`
+--
+
+INSERT INTO `activite` (`Id_ACTIVITE`, `description`, `heure_debut`, `heure_fin`, `id_journee`, `nombreMedecinActuel`, `renouveler`, `derniere_minute`, `valide`, `Id_Medecin`, `Id_TYPE_ACTIVITE`) VALUES
+(1, 'test', 4, 8, 2, 0, 0, 0, NULL, NULL, 'A'),
+(2, 'test', 4, 8, 3, 0, 0, 0, NULL, NULL, 'A'),
+(3, 'test', 4, 8, 4, 0, 0, 0, NULL, NULL, 'A'),
+(4, 'test', 4, 8, 5, 0, 0, 0, NULL, NULL, 'A'),
+(5, 'test', 4, 8, 6, 0, 0, 0, NULL, NULL, 'A'),
+(6, 'test', 4, 8, 7, 0, 0, 0, NULL, NULL, 'A'),
+(7, 'test', 4, 8, 8, 0, 0, 0, NULL, NULL, 'A'),
+(8, 'test', 4, 8, 9, 0, 0, 0, NULL, NULL, 'A');
 
 -- --------------------------------------------------------
 
@@ -50,14 +70,14 @@ CREATE TABLE IF NOT EXISTS `activite` (
 
 DROP TABLE IF EXISTS `effectuer`;
 CREATE TABLE IF NOT EXISTS `effectuer` (
-  `Id_Medecin` int(11) NOT NULL,
-  `Id_ACTIVITE` int(11) NOT NULL,
-  `idJournee` int(100) NOT NULL,
-  `idHeure` int(24) NOT NULL,
+  `Id_Medecin` int NOT NULL,
+  `Id_ACTIVITE` int NOT NULL,
+  `idJournee` int NOT NULL,
+  `idHeure` int NOT NULL,
   `astreinte__o_n_` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`Id_Medecin`,`Id_ACTIVITE`),
   KEY `Id_ACTIVITE` (`Id_ACTIVITE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -67,9 +87,9 @@ CREATE TABLE IF NOT EXISTS `effectuer` (
 
 DROP TABLE IF EXISTS `heure`;
 CREATE TABLE IF NOT EXISTS `heure` (
-  `idHeure` int(2) NOT NULL AUTO_INCREMENT,
+  `idHeure` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`idHeure`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `heure`
@@ -109,20 +129,28 @@ INSERT INTO `heure` (`idHeure`) VALUES
 
 DROP TABLE IF EXISTS `journee`;
 CREATE TABLE IF NOT EXISTS `journee` (
-  `idJournee` int(100) NOT NULL AUTO_INCREMENT,
-  `jourSemaine` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `jourDate` int(2) NOT NULL,
-  `mois` int(2) NOT NULL,
-  `annee` int(4) NOT NULL,
+  `idJournee` int NOT NULL AUTO_INCREMENT,
+  `jourSemaine` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `jourDate` int NOT NULL,
+  `mois` int NOT NULL,
+  `annee` int NOT NULL,
   PRIMARY KEY (`idJournee`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `journee`
 --
 
 INSERT INTO `journee` (`idJournee`, `jourSemaine`, `jourDate`, `mois`, `annee`) VALUES
-(1, 'Vendredi', 8, 4, 2022);
+(1, 'Vendredi', 8, 4, 2022),
+(2, 'Mercredi', 4, 5, 2022),
+(3, 'Lundi', 2, 5, 2022),
+(4, 'Mardi', 3, 5, 2022),
+(5, 'Mercredi', 4, 5, 2022),
+(6, 'Jeudi', 5, 5, 2022),
+(7, 'Vendredi', 6, 5, 2022),
+(8, 'Samedi', 7, 5, 2022),
+(9, 'Dimanche', 8, 5, 2022);
 
 -- --------------------------------------------------------
 
@@ -132,19 +160,19 @@ INSERT INTO `journee` (`idJournee`, `jourSemaine`, `jourDate`, `mois`, `annee`) 
 
 DROP TABLE IF EXISTS `medecin`;
 CREATE TABLE IF NOT EXISTS `medecin` (
-  `idMedecin` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `prenom` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `coordonnees` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `mail` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `adresse` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `nombreMinimumGarde` int(2) DEFAULT '0',
-  `nombreMaximumGarde` int(2) DEFAULT '0',
-  `login` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `mdp` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `idMedecin` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `prenom` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `coordonnees` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mail` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `adresse` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nombreMinimumGarde` int DEFAULT '0',
+  `nombreMaximumGarde` int DEFAULT '0',
+  `login` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mdp` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idMedecin`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `medecin`
@@ -164,9 +192,9 @@ INSERT INTO `medecin` (`idMedecin`, `nom`, `prenom`, `coordonnees`, `mail`, `adr
 
 DROP TABLE IF EXISTS `responsable_planning`;
 CREATE TABLE IF NOT EXISTS `responsable_planning` (
-  `idMedecin` int(11) NOT NULL,
+  `idMedecin` int NOT NULL,
   PRIMARY KEY (`idMedecin`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `responsable_planning`
@@ -184,11 +212,11 @@ INSERT INTO `responsable_planning` (`idMedecin`) VALUES
 
 DROP TABLE IF EXISTS `secteur`;
 CREATE TABLE IF NOT EXISTS `secteur` (
-  `Id_SECTEUR` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `nomSecteur` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `nombreMedecinRequis` int(3) DEFAULT '0',
+  `Id_SECTEUR` varchar(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nomSecteur` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nombreMedecinRequis` int DEFAULT '0',
   PRIMARY KEY (`Id_SECTEUR`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `secteur`
@@ -206,14 +234,14 @@ INSERT INTO `secteur` (`Id_SECTEUR`, `nomSecteur`, `nombreMedecinRequis`) VALUES
 
 DROP TABLE IF EXISTS `stock`;
 CREATE TABLE IF NOT EXISTS `stock` (
-  `Id_Medecin` int(11) NOT NULL,
-  `mois` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `annee` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `nombreGarde` int(2) DEFAULT '0',
-  `nombreAstreinte` int(2) DEFAULT '0',
-  `nomHeuresTravaille` int(11) NOT NULL,
+  `Id_Medecin` int NOT NULL,
+  `mois` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `annee` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nombreGarde` int DEFAULT '0',
+  `nombreAstreinte` int DEFAULT '0',
+  `nomHeuresTravaille` int NOT NULL,
   PRIMARY KEY (`Id_Medecin`,`mois`,`annee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -223,14 +251,14 @@ CREATE TABLE IF NOT EXISTS `stock` (
 
 DROP TABLE IF EXISTS `type_activite`;
 CREATE TABLE IF NOT EXISTS `type_activite` (
-  `Id_TYPE_ACTIVITE` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `abreviation` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `libelle_type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `description_type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `secteur` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Id_TYPE_ACTIVITE` varchar(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `abreviation` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `libelle_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `secteur` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`Id_TYPE_ACTIVITE`),
   KEY `secteur` (`secteur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `type_activite`
@@ -249,7 +277,10 @@ INSERT INTO `type_activite` (`Id_TYPE_ACTIVITE`, `abreviation`, `libelle_type`, 
 --
 ALTER TABLE `activite`
   ADD CONSTRAINT `activite_ibfk_1` FOREIGN KEY (`Id_Medecin`) REFERENCES `responsable_planning` (`idMedecin`),
-  ADD CONSTRAINT `activite_ibfk_3` FOREIGN KEY (`Id_TYPE_ACTIVITE`) REFERENCES `type_activite` (`Id_TYPE_ACTIVITE`);
+  ADD CONSTRAINT `activite_ibfk_3` FOREIGN KEY (`Id_TYPE_ACTIVITE`) REFERENCES `type_activite` (`Id_TYPE_ACTIVITE`),
+  ADD CONSTRAINT `activite_ibfk_4` FOREIGN KEY (`id_journee`) REFERENCES `journee` (`idJournee`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `activite_ibfk_6` FOREIGN KEY (`heure_debut`) REFERENCES `heure` (`idHeure`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `activite_ibfk_7` FOREIGN KEY (`heure_fin`) REFERENCES `heure` (`idHeure`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Contraintes pour la table `effectuer`
