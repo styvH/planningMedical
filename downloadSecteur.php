7727f913f6357a0bce9b23674501ba2a18c8c7e0
styvH <100362675+styvH@users.noreply.github.com>
2022-05-26 18:37:46 -0400

    update meta utf8 excel download

--- downloadSecteur.php
+++ downloadSecteur.php
@@ -1,4 +1,14 @@
-<?php 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
 include('excelWriterDownload.php');
 
  $link=mysqli_connect("localhost","root","","planning_medical");
@@ -16,7 +26,7 @@ $ligne = mysqli_fetch_assoc($result);
 	
 
 	while($ligne)
-	{
+	{ 
 		
 		
 		echo"<tr>";
@@ -49,3 +59,5 @@ mysqli_close($link);
 
 	echo"</table>";
 ?>
+</body>
+</html>
